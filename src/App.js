8cf1c7eec2216f6b03120e6401e8b8f51066a07d
MicahDunn <micahwdunn@gmail.com>
2019-04-10 02:49:25 +0000

    Nano React App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./components/navbar.js"
+import MainLogo from "./components/mainlogo.js"
+
+export default () => (
+  <>
+    <Navbar/>
+    <MainLogo/>
+  </>
+);
---
+++ src/components/mainlogo.js
@@ -0,0 +1,6 @@
+import React from "react";
+import logo from '../assets/md-logo.png';
+
+export default () => (
+<><div className="ced"><img src={logo} id="main-logo"/></div></>
+)
