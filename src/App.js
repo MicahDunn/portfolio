de3296b594e9dbcab24c86335c7a82cf0ee0ad36
MicahDunn <micahwdunn@gmail.com>
2019-04-09 20:26:25 -0700

    Fixed logo & added greeting component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar.js"
 import MainLogo from "./components/mainlogo.js"
+import Greeting from "./components/greeting.js"
 
 export default () => (
   <>
     <Navbar/>
+    <Greeting/>
     <MainLogo/>
   </>
 );
---
+++ src/components/greeting.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default () => (
+<><div className="led"><p className="greeting">Hi, my name is Micah<br/>and I like to build things.</p></div></>
+)
