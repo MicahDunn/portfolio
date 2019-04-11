b6070d887a5e82c8f7a4dd5acb62e13179fdefcf
MicahDunn <micahwdunn@gmail.com>
2019-04-10 18:59:16 -0700

    more icons

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,9 @@ import logo from '../assets/md-logo.png';
 import './navbar.css';
 import IosHomeOutline from "react-ionicons/lib/IosHomeOutline"
 import IosContactOutline from "react-ionicons/lib/IosContactOutline"
-
+import IosSettingsOutline from "react-ionicons/lib/IosSettingsOutline"
+import IosPaperOutline from "react-ionicons/lib/IosPaperOutline"
+import LogoGithub from "react-ionicons/lib/LogoGithub"
 export default () => (
 <> <nav><div className="navbar"><div className="top-logo"><img className="small-logo" src={logo}/></div><div className="nav-icons">
 <div className="helper">
@@ -14,9 +16,21 @@ export default () => (
 <li>
 <IosContactOutline className="nav-ionicons" />
 </li>
+<li>
+<IosSettingsOutline className="nav-ionicons" />
+</li>
+<li>
+<IosPaperOutline className="nav-ionicons" />
+</li>
+</ul>
+<div className="spacer"/>
+<ul>
+<li>
+<LogoGithub className="nav-ionicons" />
+</li>
 </ul>
 </div>
 </div>
 </div>
-</nav></>
+</nav> </>
 )
